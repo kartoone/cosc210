3b98b2ac09809b33365b8d3d48be821237de03c4
Brian Toone <brtoone@samford.edu>
2025-09-10 14:48:34 -0500

    renamed and expanded

--- src/InputExample.java
+++ src/InputExample.java
@@ -4,32 +4,46 @@ import java.util.Scanner;
 public class InputExample {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.println("What is your name?");
-        String name = in.nextLine();
-        if (name.length() > 15) {
-            System.out.println("too long");
-        } else {
-            System.out.println(name);
-            int nameLength = name.length();
-            if (nameLength > 50) {
-                System.out.println("too long!");
-            } else if (nameLength > 45) {
-                System.out.println("warning, this name is almost too long");
-            } else if (nameLength > 30) {
-                System.out.println("caution, approaching limit");
-            } else {
-                System.out.println(name);
-            }
-        }
+        // System.out.println("What is your name?");
+        // String name = in.nextLine();
+        // if (name.length() > 15) {
+        //     System.out.println("too long");
+        // } else {
+        //     System.out.println(name);
+        //     int nameLength = name.length();
+        //     if (nameLength > 50) {
+        //         System.out.println("too long!");
+        //     } else if (nameLength > 45) {
+        //         System.out.println("warning, this name is almost too long");
+        //     } else if (nameLength > 30) {
+        //         System.out.println("caution, approaching limit");
+        //     } else {
+        //         System.out.println(name);
+        //     }
+        // }
 
-        System.out.println("Choose from the following options:");
-        System.out.println("1. Enter your height");
-        System.out.println("2. Enter your eye color");
-        System.out.println("3. Enter your hair color");
-        System.out.println("4. Quit");
-        System.out.print("Choice? ");
+        // System.out.println("Choose from the following options:");
+        // System.out.println("1. Enter your height");
+        // System.out.println("2. Enter your eye color");
+        // System.out.println("3. Enter your hair color");
+        // System.out.println("4. Quit");
+        // System.out.print("Choice? ");
 
-        int choice = in.nextInt();
+        // int choice = in.nextInt();
+
+        // Instead of a menu, we could INSTEAD ask for multiple things at once!
+        System.out.println("Enter length width depth (separated by spaces):");
+        double length = in.nextDouble();
+        double width = in.nextDouble();
+        double depth = in.nextDouble();
+        double volume = length * width * depth;
+        System.out.println("The volume of the box is: " + volume);
+
+        System.out.print(length + ",");
+        System.out.print(width + ",");
+        System.out.print(depth + ",");
+        System.out.println();
+
 
     }
 }
