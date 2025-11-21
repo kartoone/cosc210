764ff070de3f67e1cfcaf8b914fe76afca27ee49
Brian Toone <brtoone@samford.edu>
2025-11-21 09:06:49 -0600

    stack interface and implementation

--- src/chapter9/ArrayStack.java
+++ src/chapter9/ArrayStack.java
@@ -1,5 +1,32 @@
 package chapter9;
 
-public class ArrayStack {
+public class ArrayStack<E> implements Stack<E> {
+
+    @SuppressWarnings("unchecked")
+    private E data[] = (E[]) new Object[100];
+    private int size = 0; // the real size of the stack
+
+    @Override
+    public void push(E e) {
+        if (size == data.length) {
+            data = java.util.Arrays.copyOf(data,data.length*2);
+        }
+        data[size++] = e;
+    }
+
+    @Override
+    public E pop() {
+        return data[--size];
+    }
+
+    @Override
+    public E top() {
+        return data[size-1];
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
 
 }
--- src/chapter9/Stack.java
+++ src/chapter9/Stack.java
@@ -1,5 +1,10 @@
 package chapter9;
 
-public interface Stack {
+public interface Stack<E> {
+
+    public void push(E e);
+    public E pop();
+    public E top();
+    public int size();
 
 }
