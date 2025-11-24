6339c68b06803c32e8709eaba71df12043f8d43f
kartoone <brtoone@samford.edu>
2025-11-24 10:24:03 -0600

    helper class for http

---
+++ src/chapter9/HttpManager.java
@@ -0,0 +1,82 @@
+package chapter9;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+// https://www.digitalocean.com/community/tutorials/java-httpurlconnection-example-java-http-request-get-post
+
+public class HttpManager {
+
+	private static final String USER_AGENT = "Mozilla/5.0";
+
+	public static void main(String[] args) throws IOException {
+		sendGET("https://www.samford.edu");
+		System.out.println("GET DONE");
+		sendPOST("https://www.samford.edu", "userName=Pankaj");
+		System.out.println("POST DONE");
+	}
+
+	public static void sendGET(String url) throws IOException {
+		URL obj = new URL(url);
+		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+		con.setRequestMethod("GET");
+		con.setRequestProperty("User-Agent", USER_AGENT);
+		int responseCode = con.getResponseCode();
+		System.out.println("GET Response Code :: " + responseCode);
+		if (responseCode == HttpURLConnection.HTTP_OK) { // success
+			BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+			String inputLine;
+			StringBuffer response = new StringBuffer();
+
+			while ((inputLine = in.readLine()) != null) {
+				response.append(inputLine);
+			}
+			in.close();
+
+			// print result
+			System.out.println(response.toString());
+		} else {
+			System.out.println("GET request did not work.");
+		}
+
+	}
+
+	public static void sendPOST(String url, String params) throws IOException {
+		URL obj = new URL(url);
+		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+		con.setRequestMethod("POST");
+		con.setRequestProperty("User-Agent", USER_AGENT);
+
+		// For POST only - START
+		con.setDoOutput(true);
+		OutputStream os = con.getOutputStream();
+		os.write(params.getBytes());
+		os.flush();
+		os.close();
+		// For POST only - END
+
+		int responseCode = con.getResponseCode();
+		System.out.println("POST Response Code :: " + responseCode);
+
+		if (responseCode == HttpURLConnection.HTTP_OK) { //success
+			BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+			String inputLine;
+			StringBuffer response = new StringBuffer();
+
+			while ((inputLine = in.readLine()) != null) {
+				response.append(inputLine);
+			}
+			in.close();
+
+			// print result
+			System.out.println(response.toString());
+		} else {
+			System.out.println("POST request did not work.");
+		}
+	}
+
+}
